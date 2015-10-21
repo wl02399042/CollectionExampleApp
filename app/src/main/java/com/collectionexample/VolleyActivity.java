161c0d2ecb15c9986890d094d54445f9403568a4
stu1002138 <stu1002138@gmail.com>
2015-10-21 20:46:12 +0800

    volleyActivity add a Button

--- app/src/main/java/com/collectionexample/VolleyActivity.java
+++ app/src/main/java/com/collectionexample/VolleyActivity.java
@@ -2,6 +2,7 @@ package com.collectionexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class VolleyActivity extends AppCompatActivity {
@@ -9,8 +10,8 @@ public class VolleyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView text = new TextView(this);
-        text.setText("volley網路存取範例");
-        setContentView(text);
+        Button button = new Button(this);
+        button.setText("volley網路存取範例");
+        setContentView(button);
     }
 }
